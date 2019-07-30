8f855b8efb2bcd0e81ce63710ad41ab941b974cd
Michael Hirn <michael.j.hirn@gmail.com>
2019-07-30 12:02:59 +0100

    refactor/*: add check-types lib

--- src/transform.js
+++ src/transform.js
@@ -1,71 +1,9 @@
-// BASE
-function isString (value) {
-  return typeof value === 'string' || value instanceof String;
-}
-
-function isNumber (value) {
-  return typeof value === 'number' && isFinite(value);
-}
-
-function isArray (value) {
-  return value && typeof value === 'object' && value.constructor === Array;
-}
-
-function isObject (value) {
-  return value && typeof value === 'object' && value.constructor === Object;
-}
-
-function isNull (value) {
-  return value === null;
-}
-
-function isRegExp (value) {
-  return value && typeof value === 'object' && value.constructor === RegExp;
-}
-
-function isDate (value) {
-  return value instanceof Date;
-}
-
-function isUndefined (value) {
-  return typeof value === 'undefined';
-}
-
-function isBoolean (value) {
-  return typeof value === 'boolean';
-}
-
-// COMPLEX
-
-function isEmptyObject (value) {
-  return isObject(value) && Object.keys(value).length === 0 && value.constructor === Object
-}
-
-function isEmptyArray (value) {
-  return isArray(value) && value.length === 0
-}
-
-function isObjectArray (value) {
-  return !isEmptyArray(value) && value.every(isObject);
-}
-
-function isStringArray (value) {
-  return !isEmptyArray(value) && value.every(isStringable);
-}
-
-function isEmpty (value) {
-  return isUndefined(value) //|| isEmptyObject(value) || isEmptyArray(value)
-}
+const check = require('check-types');
 
 function isStringable (value) {
-  return isString(value) ||
-    isNumber(value) ||
-    isBoolean(value) ||
-    isNull(value) ||
-    isRegExp(value) ||
-    isDate(value)
+  return check.string(value) || check.number(value) || check.boolean(value) ||
+    check.null(value) || value.constructor === RegExp || check.date(value)
 }
-
 
 class RlayTransform {
   static generateLabel (client, pathRN) {
@@ -159,7 +97,7 @@ class RlayTransform {
   }
 
   static _getKeys (value) {
-    if (isArray(value)) {
+    if (check.array(value)) {
       return Array.from(new Set(...value.map(RlayTransform._getKeys)));
     }
     return Object.keys(value);
@@ -169,7 +107,7 @@ class RlayTransform {
   static _assignRlayTransformPrefix (prefix) {
     const RLAY_TRANSFORM_PREFIX = 'RlayTransform';
     // check that the prefix is an array already
-    if (!isArray(prefix)) return this._assignRlayTransformPrefix([prefix]);
+    if (check.not.array(prefix)) return this._assignRlayTransformPrefix([prefix]);
     // check if it has the Rlay Transform Prefix already
     if (prefix[0] === RLAY_TRANSFORM_PREFIX) {
       // ok, return it as is
@@ -191,7 +129,7 @@ class RlayTransform {
     this._initIndexMap();
     const arr = [];
     const properties = [];
-    if (isObject(json)) {
+    if (check.object(json)) {
       const objectKeys = Object.keys(json);
       // create classes for that object
       const cLabel  = this.generateLabel(client, pathRN);
@@ -201,7 +139,7 @@ class RlayTransform {
       properties.push(ca);
       objectKeys.forEach(key => {
         const value = json[key];
-        if (!isEmpty(value)) {
+        if (check.not.undefined(value)) {
           if (isStringable(value)) {
             // normal single value DataProperty
             const label = this.generateLabel(client, [...pathRN, key]);
@@ -209,7 +147,9 @@ class RlayTransform {
             const dpa   = this.generateDataPropertyAssertion(client, dp, value);
             arr.push(...[label, dp, dpa]);
             properties.push(dpa);
-          } else if (isArray(value) && isStringArray(value)) {
+          } else if (check.array(value) &&
+            check.nonEmptyArray(value) &&
+            check.all(value.map(isStringable))) {
             // an array of single value DataProperties
             // note: this does not capture or preserve the order of the array's elements
             const label = this.generateLabel(client, [...pathRN, key]);
@@ -217,7 +157,7 @@ class RlayTransform {
             const dpas  = value.map(v => this.generateDataPropertyAssertion(client, dp, v));
             arr.push(...[label, dp, ...dpas]);
             properties.push(...dpas);
-          } else if (isObject(value)) {
+          } else if (check.object(value)) {
             // a single value ObjectProperty
             const entities = this.toRlayEntities(client, [...pathRN, key], value);
             const i     = entities.filter(e => e.type === 'Individual').shift();
@@ -226,7 +166,9 @@ class RlayTransform {
             const opa   = this.generateObjectPropertyAssertion(client, op, i);
             arr.push(...[label, op, opa, ...entities]);
             properties.push(opa);
-          } else if (isArray(value) && isObjectArray(value)) {
+          } else if (check.array(value) &&
+            check.nonEmptyArray(value) &&
+            check.all(value.map(check.object.bind(check)))) {
             // an array of single value ObjectProperties
             // note: this does not capture or preserve the order of the array's elements
             const label = this.generateLabel(client, [...pathRN, key]);
@@ -239,7 +181,7 @@ class RlayTransform {
             });
             arr.push(...[label, op, ...opas]);
             properties.push(...opas);
-          } else if (isArray(value) && !isEmptyArray(value)) {
+          } else if (check.array(value) && check.nonEmptyArray(value)) {
             // an array with mixed elements (DataProperty/ObjectProperty)
             // note: this does not capture or preserve the order of the array's elements
             const label = this.generateLabel(client, [...pathRN, key]);
@@ -248,7 +190,7 @@ class RlayTransform {
             const xpas  = value.map(v => {
               if (isStringable(v)) {
                 return this.generateDataPropertyAssertion(client, dp, v);
-              } else if (isObject(v)) {
+              } else if (check.object(v)) {
                 const entities = this.toRlayEntities(client, [...pathRN, key], v);
                 const i   = entities.filter(e => e.type === 'Individual').shift();
                 arr.push(...entities);
